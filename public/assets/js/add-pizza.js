47257486e8a83061f53c0b60039934d481af99d4
Patrick Lago <Patrick@Dinas-Air.attlocal.net>
2021-07-13 23:04:31 -0700

    finished lesson 4

--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -67,7 +67,9 @@ const handlePizzaSubmit = event => {
     })
     .catch(err => {
       console.log(err);
+      saveRecord(formData);
     });
+    
 };
 
 $pizzaForm.addEventListener('submit', handlePizzaSubmit);
